3a6596fcae01b31a0ca0b9a4af8006cbb9987dd6
Reuben Fletcher-Costin <reuben.fletcher-costin@anz.com>
2020-11-23 12:50:03 +1100

    pkg/logging tests - refactor to appease linter

--- logging/context_test.go
+++ logging/context_test.go
@@ -27,133 +27,140 @@ func TestGetLoggerFromContext(t *testing.T) {
 	// assert.Equal(t, logger, loggerFromCtx)
 }
 
-func TestLogFuncs(t *testing.T) {
-	t.Run("Info", func(t *testing.T) {
-		buf := bytes.Buffer{}
-		logger := logging.New(&buf)
-		ctx := logger.ToContext(context.Background())
-		logging.Info(ctx).Msg("Hello World")
-		assert.Contains(t, buf.String(), "Hello World")
-	})
-	t.Run("InfoUsesGlobalIfNoLogger", func(t *testing.T) {
-		buf := bytes.Buffer{}
-		logging.SetGlobalLogContext(logging.New(&buf).With())
-		logging.Info(context.Background()).Msg("Hello World")
-		assert.Contains(t, buf.String(), `"globalLogger":true`)
-	})
+func TestLogFuncInfo(t *testing.T) {
+	buf := bytes.Buffer{}
+	logger := logging.New(&buf)
+	ctx := logger.ToContext(context.Background())
+	logging.Info(ctx).Msg("Hello World")
+	assert.Contains(t, buf.String(), "Hello World")
+}
 
-	t.Run("Debug", func(t *testing.T) {
-		buf := bytes.Buffer{}
-		logger := logging.New(&buf).WithLevel(logging.DebugLevel)
-		ctx := logger.ToContext(context.Background())
-		logging.Debug(ctx).Msg("Hello World")
-		assert.Contains(t, buf.String(), "Hello World")
-	})
-	t.Run("DebugUsesGlobalIfNoLogger", func(t *testing.T) {
-		buf := bytes.Buffer{}
-		logging.SetGlobalLogContext(logging.New(&buf).WithLevel(logging.DebugLevel).With())
-		logging.Debug(context.Background()).Msg("Hello World")
-		assert.Contains(t, buf.String(), `"globalLogger":true`)
-	})
+func TestLogFuncInfoUsesGlobalIfNoLogger(t *testing.T) {
+	buf := bytes.Buffer{}
+	logging.SetGlobalLogContext(logging.New(&buf).With())
+	logging.Info(context.Background()).Msg("Hello World")
+	assert.Contains(t, buf.String(), `"globalLogger":true`)
+}
 
-	t.Run("Error", func(t *testing.T) {
-		buf := bytes.Buffer{}
-		logger := logging.New(&buf)
-		ctx := logger.ToContext(context.Background())
-		logging.Error(ctx, fmt.Errorf("Bad things")).Msg("Hello World")
-		assert.Contains(t, buf.String(), "Hello World")
-		assert.Contains(t, buf.String(), "Bad things")
-	})
-	t.Run("ErrorUsesGlobalIfNoLogger", func(t *testing.T) {
-		buf := bytes.Buffer{}
-		logging.SetGlobalLogContext(logging.New(&buf).With())
-		logging.Error(context.Background(), fmt.Errorf("Bad things")).Msg("Hello World")
-		assert.Contains(t, buf.String(), `"globalLogger":true`)
-	})
-	t.Run("ContextFromContextWithDur", func(t *testing.T) {
-		buf := bytes.Buffer{}
-		logger := logging.New(&buf).WithLevel(logging.DebugLevel)
-		ctx := logger.ToContext(context.Background())
-		loggerContext := logging.ContextFromContext(ctx)
-		loggerContext = loggerContext.WithDur("sale_duration", 72*time.Hour)
-		ctx = loggerContext.ToContext(ctx)
-		logging.Debug(ctx).Msg("Hello World")
-		assert.Contains(t, buf.String(), "Hello World")
-		assert.Contains(t, buf.String(), "sale_duration")
-		assert.Contains(t, buf.String(), "259200000")
-	})
-	t.Run("ContextFromContextWithTime", func(t *testing.T) {
-		buf := bytes.Buffer{}
-		logger := logging.New(&buf).WithLevel(logging.DebugLevel)
-		ctx := logger.ToContext(context.Background())
-		loggerContext := logging.ContextFromContext(ctx)
-		loggerContext = loggerContext.WithTime("sale_start", time.Date(2020, time.November, 23, 0, 0, 0, 0, time.UTC))
-		ctx = loggerContext.ToContext(ctx)
-		logging.Debug(ctx).Msg("Hello World")
-		assert.Contains(t, buf.String(), "Hello World")
-		assert.Contains(t, buf.String(), "sale_start")
-		assert.Contains(t, buf.String(), "2020-11-23T00:00:00Z")
-	})
-	t.Run("ContextFromContextWithStr", func(t *testing.T) {
-		buf := bytes.Buffer{}
-		logger := logging.New(&buf).WithLevel(logging.DebugLevel)
-		ctx := logger.ToContext(context.Background())
-		loggerContext := logging.ContextFromContext(ctx)
-		loggerContext = loggerContext.WithStr("fruit", "banana")
-		ctx = loggerContext.ToContext(ctx)
-		logging.Debug(ctx).Msg("Hello World")
-		assert.Contains(t, buf.String(), "Hello World")
-		assert.Contains(t, buf.String(), "fruit")
-		assert.Contains(t, buf.String(), "banana")
-	})
-	t.Run("ContextFromContextWithInt", func(t *testing.T) {
-		buf := bytes.Buffer{}
-		logger := logging.New(&buf).WithLevel(logging.DebugLevel)
-		ctx := logger.ToContext(context.Background())
-		loggerContext := logging.ContextFromContext(ctx)
-		loggerContext = loggerContext.WithInt("price", 100)
-		ctx = loggerContext.ToContext(ctx)
-		logging.Debug(ctx).Msg("Hello World")
-		assert.Contains(t, buf.String(), "Hello World")
-		assert.Contains(t, buf.String(), "price")
-		assert.Contains(t, buf.String(), "100")
-	})
-	t.Run("ContextFromContextWithDict", func(t *testing.T) {
-		buf := bytes.Buffer{}
-		logger := logging.New(&buf).WithLevel(logging.DebugLevel)
-		ctx := logger.ToContext(context.Background())
-		loggerContext := logging.ContextFromContext(ctx)
-		loggerContext = loggerContext.WithDict("specials", logging.Dict().
-			Str("bundle", "bunch").
-			Int("price", 299))
-		ctx = loggerContext.ToContext(ctx)
-		logging.Debug(ctx).Msg("Hello World")
-		assert.Contains(t, buf.String(), "Hello World")
-		assert.Contains(t, buf.String(), "specials")
-		assert.Contains(t, buf.String(), "bundle")
-		assert.Contains(t, buf.String(), "bunch")
-		assert.Contains(t, buf.String(), "price")
-		assert.Contains(t, buf.String(), "299")
-	})
-	t.Run("ContextFromContextWithArray", func(t *testing.T) {
-		buf := bytes.Buffer{}
-		logger := logging.New(&buf).WithLevel(logging.DebugLevel)
-		ctx := logger.ToContext(context.Background())
-		loggerContext := logging.ContextFromContext(ctx)
-		loggerContext = loggerContext.WithStr("fruit", "banana")
-		loggerContext = loggerContext.WithInt("price", 100)
-		loggerContext = loggerContext.WithDict("specials", logging.Dict().
-			Str("bundle", "bunch").
-			Int("price", 299))
-		loggerContext = loggerContext.WithArray("sales", logging.Array().Int(3).Int(2).Int(1).Int(9).Int(0))
-		ctx = loggerContext.ToContext(ctx)
-		logging.Debug(ctx).Msg("Hello World")
-		assert.Contains(t, buf.String(), "Hello World")
-		assert.Contains(t, buf.String(), "sales")
-		assert.Contains(t, buf.String(), "3")
-		assert.Contains(t, buf.String(), "2")
-		assert.Contains(t, buf.String(), "1")
-		assert.Contains(t, buf.String(), "9")
-		assert.Contains(t, buf.String(), "0")
-	})
+func TestLogFuncDebug(t *testing.T) {
+	buf := bytes.Buffer{}
+	logger := logging.New(&buf).WithLevel(logging.DebugLevel)
+	ctx := logger.ToContext(context.Background())
+	logging.Debug(ctx).Msg("Hello World")
+	assert.Contains(t, buf.String(), "Hello World")
+}
+
+func TestLogFuncDebugUsesGlobalIfNoLogger(t *testing.T) {
+	buf := bytes.Buffer{}
+	logging.SetGlobalLogContext(logging.New(&buf).WithLevel(logging.DebugLevel).With())
+	logging.Debug(context.Background()).Msg("Hello World")
+	assert.Contains(t, buf.String(), `"globalLogger":true`)
+}
+
+func TestLogFuncError(t *testing.T) {
+	buf := bytes.Buffer{}
+	logger := logging.New(&buf)
+	ctx := logger.ToContext(context.Background())
+	logging.Error(ctx, fmt.Errorf("Bad things")).Msg("Hello World")
+	assert.Contains(t, buf.String(), "Hello World")
+	assert.Contains(t, buf.String(), "Bad things")
+}
+
+func TestLogFuncErrorUsesGlobalIfNoLogger(t *testing.T) {
+	buf := bytes.Buffer{}
+	logging.SetGlobalLogContext(logging.New(&buf).With())
+	logging.Error(context.Background(), fmt.Errorf("Bad things")).Msg("Hello World")
+	assert.Contains(t, buf.String(), `"globalLogger":true`)
+}
+
+func TestLogContextFromContextWithDur(t *testing.T) {
+	buf := bytes.Buffer{}
+	logger := logging.New(&buf).WithLevel(logging.DebugLevel)
+	ctx := logger.ToContext(context.Background())
+	loggerContext := logging.ContextFromContext(ctx)
+	loggerContext = loggerContext.WithDur("sale_duration", 72*time.Hour)
+	ctx = loggerContext.ToContext(ctx)
+	logging.Debug(ctx).Msg("Hello World")
+	assert.Contains(t, buf.String(), "Hello World")
+	assert.Contains(t, buf.String(), "sale_duration")
+	assert.Contains(t, buf.String(), "259200000")
+}
+
+func TestLogContextFromContextWithTime(t *testing.T) {
+	buf := bytes.Buffer{}
+	logger := logging.New(&buf).WithLevel(logging.DebugLevel)
+	ctx := logger.ToContext(context.Background())
+	loggerContext := logging.ContextFromContext(ctx)
+	loggerContext = loggerContext.WithTime("sale_start", time.Date(2020, time.November, 23, 0, 0, 0, 0, time.UTC))
+	ctx = loggerContext.ToContext(ctx)
+	logging.Debug(ctx).Msg("Hello World")
+	assert.Contains(t, buf.String(), "Hello World")
+	assert.Contains(t, buf.String(), "sale_start")
+	assert.Contains(t, buf.String(), "2020-11-23T00:00:00Z")
+}
+
+func TestLogContextFromContextWithStr(t *testing.T) {
+	buf := bytes.Buffer{}
+	logger := logging.New(&buf).WithLevel(logging.DebugLevel)
+	ctx := logger.ToContext(context.Background())
+	loggerContext := logging.ContextFromContext(ctx)
+	loggerContext = loggerContext.WithStr("fruit", "banana")
+	ctx = loggerContext.ToContext(ctx)
+	logging.Debug(ctx).Msg("Hello World")
+	assert.Contains(t, buf.String(), "Hello World")
+	assert.Contains(t, buf.String(), "fruit")
+	assert.Contains(t, buf.String(), "banana")
+}
+
+func TestLogContextFromContextWithInt(t *testing.T) {
+	buf := bytes.Buffer{}
+	logger := logging.New(&buf).WithLevel(logging.DebugLevel)
+	ctx := logger.ToContext(context.Background())
+	loggerContext := logging.ContextFromContext(ctx)
+	loggerContext = loggerContext.WithInt("price", 100)
+	ctx = loggerContext.ToContext(ctx)
+	logging.Debug(ctx).Msg("Hello World")
+	assert.Contains(t, buf.String(), "Hello World")
+	assert.Contains(t, buf.String(), "price")
+	assert.Contains(t, buf.String(), "100")
+}
+
+func TestLogContextFromContextWithDict(t *testing.T) {
+	buf := bytes.Buffer{}
+	logger := logging.New(&buf).WithLevel(logging.DebugLevel)
+	ctx := logger.ToContext(context.Background())
+	loggerContext := logging.ContextFromContext(ctx)
+	loggerContext = loggerContext.WithDict("specials", logging.Dict().
+		Str("bundle", "bunch").
+		Int("price", 299))
+	ctx = loggerContext.ToContext(ctx)
+	logging.Debug(ctx).Msg("Hello World")
+	assert.Contains(t, buf.String(), "Hello World")
+	assert.Contains(t, buf.String(), "specials")
+	assert.Contains(t, buf.String(), "bundle")
+	assert.Contains(t, buf.String(), "bunch")
+	assert.Contains(t, buf.String(), "price")
+	assert.Contains(t, buf.String(), "299")
+}
+
+func TestLogContextFromContextWithArray(t *testing.T) {
+	buf := bytes.Buffer{}
+	logger := logging.New(&buf).WithLevel(logging.DebugLevel)
+	ctx := logger.ToContext(context.Background())
+	loggerContext := logging.ContextFromContext(ctx)
+	loggerContext = loggerContext.WithStr("fruit", "banana")
+	loggerContext = loggerContext.WithInt("price", 100)
+	loggerContext = loggerContext.WithDict("specials", logging.Dict().
+		Str("bundle", "bunch").
+		Int("price", 299))
+	loggerContext = loggerContext.WithArray("sales", logging.Array().Int(3).Int(2).Int(1).Int(9).Int(0))
+	ctx = loggerContext.ToContext(ctx)
+	logging.Debug(ctx).Msg("Hello World")
+	assert.Contains(t, buf.String(), "Hello World")
+	assert.Contains(t, buf.String(), "sales")
+	assert.Contains(t, buf.String(), "3")
+	assert.Contains(t, buf.String(), "2")
+	assert.Contains(t, buf.String(), "1")
+	assert.Contains(t, buf.String(), "9")
+	assert.Contains(t, buf.String(), "0")
 }
